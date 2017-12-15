02c993c2eb165bdbdff39b2b7e143a575c5af9b1
Yoyoeye <yoyoeye@yoyoeye-TravelMate-2480.(none)>
2017-12-15 18:23:45 +0200

    Starting app

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var APP_DIR = path.resolve(__dirname, 'src/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    devServer: {
+        proxy: [{
+            path: '/api/',
+            target: 'http://localhost:3001'
+        }],
+        historyApiFallback: true
+    },
+     module : {
+      loaders : [
+            {
+                test: /\.js/,
+                loaders: ['babel-loader'],
+                include: APP_DIR
+            },
+            {
+                test: /\.css/,
+                loaders: ['style-loader', 'css-loader'],
+            }
+     ]
+   }
+};
+
+module.exports = config;
